a1cdde6964c16e100efddc250094e9d98cf42c7c
gjsjohnmurray <johnm@georgejames.com>
2023-05-16 17:13:55 +0100

    Implement debugging in client-side mode (fix #1)

--- src/debugTrackerFactory.ts
+++ src/debugTrackerFactory.ts
@@ -4,6 +4,10 @@ import { DebugTracker } from './debugTracker';
 export class DebugTrackerFactory implements vscode.DebugAdapterTrackerFactory {
 
   createDebugAdapterTracker(session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterTracker> {
+    if (session.configuration.name.split(':')[0] === 'LocalTests.Preload') {
+      // No need to track the debug session which preloads the UT classes
+      return undefined;
+    }
     return new DebugTracker(session);
   }
 }
